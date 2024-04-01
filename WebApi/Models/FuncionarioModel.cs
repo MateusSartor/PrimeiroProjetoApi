db961040786c26e6ad430358079ce467b96b6fa3
Mateus_Sartor <mateus.sartor5@gmail.com>
2024-04-01 10:24:21 -0300

    first commit

---
+++ WebApi/Models/FuncionarioModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+using WebApi.Enums;
+
+namespace WebApi.Models
+{
+    public class FuncionarioModel
+    {
+            [Key]
+            public int Id { get; set; }  
+            public string Nome { get; set; }
+            public string Sobrenome { get; set; }
+            public DepartamentoEnum Dapartamento { get; set; }
+            public bool Ativo { get; set; }
+            public TurnoEnum Turno { get; set;}
+            public DateTime DataDeCriacao { get; set; } = DateTime.Now;
+            public DateTime DataDeAlteracao { get; set; } = DateTime.Now;
+
+
+
+    }
+
+}
